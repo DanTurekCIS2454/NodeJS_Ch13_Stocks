f1fe1067a662b962bb1cad1464dbad1898c93a43
Dan-Turek <turek.daniel@student.oaklandcc.edu>
2025-10-27 17:24:22 -0400

    Objective 1 Done
    read file and show in API GET

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 //server.js
 
 //required node js modules
-const http = require('http');
+//const http = require('http'); //http server replaced by express
+const fs = require('fs'); //file system
+const path = require('path'); //directory structure
+const express = require('express'); //api routes
 
-const server = http.createServer(function (request, response)
+//run instance
+const app = express();
+
+//load data
+const dataPath = path.join(__dirname, 'data', 'companies-data.json');
+
+//store stocks in var 
+let stocks;
+fs.readFile(dataPath, (err, data) =>
 {
-    response.writeHead(200, {"content-type": "text/plain"});
-    response.write("Hello");
-    response.end();
-}
-)
+    if (err)
+        console.log('Unable to read stocks file');
+    else
+        stocks = JSON.parse(data);
+})
+
+//replace basic server with express app
+app.get('/', (request, response) => {
+    response.json(stocks)
+});
 
 const port = 8080;
-server.listen(port);
+app.listen(port, () => {
+    console.log("Server running at port=" + port);
+})
 
-console.log("Server running at port=" + port);
+//remove http server since now redundant by express app
+
